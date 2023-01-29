c78eead2a054e23178147644cb6ce30d16876219
Robinson <robinson@dorkbox.com>
2023-01-29 14:26:11 +0100

    added jpms support

---
+++ src9/module-info.java
@@ -0,0 +1,8 @@
+module dorkbox.tweenEngine {
+    exports dorkbox.tweenEngine;
+
+    requires transitive dorkbox.updates;
+    requires transitive dorkbox.objectpool;
+
+    requires transitive kotlin.stdlib;
+}
